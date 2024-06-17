33bf989f796b2b08eb34c2660af33d225f5c8072
José Teles <josetelesmaciel@gmail.com>
2024-06-17 12:30:36 -0300

    refactor: Update TestsPage component to include drag and drop functionality
    This commit updates the TestsPage component to include drag and drop functionality using the @hello-pangea/dnd library. It enhances the user experience and improves the interactivity of the TestsPage component.

--- src/app/tests/page.tsx
+++ src/app/tests/page.tsx
@@ -2,6 +2,38 @@
 import { DragDropContext, Draggable, Droppable, DropResult } from "@hello-pangea/dnd"
 import { useState } from "react"
 
+const RowSection: React.FC<{id: number}> = ({id}) => {
+    return (
+        <section className="inline-flex border border-slate-300 w-full">
+        {Array.from({ length: 4 }).map((_, index) => (
+            <Droppable key={index} droppableId={`droppable-${id}-${index}`}>
+                {(provided) => (
+                    <div
+                        className="border p-3 mb-5 rounded-lg user-select-none w-3/12"
+                        ref={provided.innerRef}
+                        {...provided.droppableProps}>
+                        {Array.from({ length: 3 }).map((_, innerIndex) => (
+                            <Draggable key={innerIndex} draggableId={`draggable-${id}-${index}-${innerIndex}`} index={innerIndex}>
+                                {(provided) => (
+                                    <div
+                                        className="bg-gray-200 p-3 mb-5 w-48 rounded-lg user-select-none"
+                                        ref={provided.innerRef}
+                                        {...provided.draggableProps}
+                                        {...provided.dragHandleProps}>
+                                        Component {index}-{innerIndex}
+                                    </div>
+                                )}
+                            </Draggable>
+                        ))}
+                        <div className="border border-gray-200 rounded-lg">{provided.placeholder}</div>
+                    </div>
+                )}
+            </Droppable>
+        ))}
+    </section>
+    )
+}
+
 const TestsPage = () => {
     const [result, setResult] = useState<DropResult | null>(null)
     const handleDragEnd = (result: DropResult) => {
@@ -34,33 +66,11 @@ const TestsPage = () => {
                         )}
                     </Droppable>
                 </aside>
-                <section className="inline-flex w-full flex-grow-0 border border-slate-300 p-4">
-                    {Array.from({ length: 4 }).map((_, index) => (
-                        <Droppable key={index} droppableId={`droppable-${index}`}>
-                            {(provided) => (
-                                <div
-                                    className="border p-3 mb-5 rounded-lg user-select-none w-3/12"
-                                    ref={provided.innerRef}
-                                    {...provided.droppableProps}>
-                                    {Array.from({ length: 3 }).map((_, innerIndex) => (
-                                        <Draggable key={innerIndex} draggableId={`draggable-${index}-${innerIndex}`} index={innerIndex}>
-                                            {(provided) => (
-                                                <div
-                                                    className="bg-gray-200 p-3 mb-5 w-48 rounded-lg user-select-none"
-                                                    ref={provided.innerRef}
-                                                    {...provided.draggableProps}
-                                                    {...provided.dragHandleProps}>
-                                                    Draggable {index}-{innerIndex}
-                                                </div>
-                                            )}
-                                        </Draggable>
-                                    ))}
-                                    <div className="border border-gray-200 rounded-lg">{provided.placeholder}</div>
-                                </div>
-                            )}
-                        </Droppable>
+                <div className="w-full flex-grow-0 p-4">
+                    {Array.from({ length: 3 }).map((_, index) => (
+                        <RowSection key={index} id={index} />
                     ))}
-                </section>
+                </div>
             </DragDropContext>
         </div>
         <div className="bg-slate-800 text-white p-3 mt-5">
