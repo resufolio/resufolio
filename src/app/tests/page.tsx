072c403311de817e83ad42be800412aff2671a5e
José Teles <josetelesmaciel@gmail.com>
2024-06-17 10:04:44 -0300

    refactor: Update TestsPage component to include drag and drop functionality
    This commit updates the TestsPage component to include drag and drop functionality using the @hello-pangea/dnd library. It adds the DragDropContext, Draggable, and Droppable components to enable dragging and dropping of elements within the page. The handleDragEnd function is implemented to handle the drag end event and update the result state accordingly. This change enhances the user experience and improves the interactivity of the TestsPage component.

---
+++ src/app/tests/page.tsx
@@ -0,0 +1,73 @@
+"use client"
+import { DragDropContext, Draggable, Droppable, DropResult } from "@hello-pangea/dnd"
+import { useState } from "react"
+
+const TestsPage = () => {
+    const [result, setResult] = useState<DropResult | null>(null)
+    const handleDragEnd = (result: DropResult) => {
+        const { destination } = result
+        if (!destination) return
+        setResult(result)
+    }
+
+    return (
+        <div>
+        <div className="bg-white p-3 flex">
+            <DragDropContext onDragEnd={handleDragEnd}>
+                <aside className="border-b w-full py-2 flex-1">
+                    <Droppable droppableId="droppable" isDropDisabled={true}>
+                        {(provided) => (
+                            <div ref={provided.innerRef} {...provided.droppableProps} className="w-[192px]">
+                                <Draggable draggableId="draggable-1" index={0}>
+                                    {(provided) => (
+                                        <div
+                                            className="bg-gray-200 p-3 mb-5 w-48 rounded-lg user-select-none"
+                                            ref={provided.innerRef}
+                                            {...provided.draggableProps}
+                                            {...provided.dragHandleProps}>
+                                            Draggable 1
+                                        </div>
+                                    )}
+                                </Draggable>
+                                {provided.placeholder}
+                            </div>
+                        )}
+                    </Droppable>
+                </aside>
+                <section className="inline-flex w-full flex-grow-0 border border-slate-300 p-4">
+                    {Array.from({ length: 4 }).map((_, index) => (
+                        <Droppable key={index} droppableId={`droppable-${index}`}>
+                            {(provided) => (
+                                <div
+                                    className="border p-3 mb-5 rounded-lg user-select-none w-3/12"
+                                    ref={provided.innerRef}
+                                    {...provided.droppableProps}>
+                                    {Array.from({ length: 3 }).map((_, innerIndex) => (
+                                        <Draggable key={innerIndex} draggableId={`draggable-${index}-${innerIndex}`} index={innerIndex}>
+                                            {(provided) => (
+                                                <div
+                                                    className="bg-gray-200 p-3 mb-5 w-48 rounded-lg user-select-none"
+                                                    ref={provided.innerRef}
+                                                    {...provided.draggableProps}
+                                                    {...provided.dragHandleProps}>
+                                                    Draggable {index}-{innerIndex}
+                                                </div>
+                                            )}
+                                        </Draggable>
+                                    ))}
+                                    <div className="border border-gray-200 rounded-lg">{provided.placeholder}</div>
+                                </div>
+                            )}
+                        </Droppable>
+                    ))}
+                </section>
+            </DragDropContext>
+        </div>
+        <div className="bg-slate-800 text-white p-3 mt-5">
+            <pre className="whitespace-pre-wrap">{JSON.stringify(result, null, 2)}</pre>
+        </div>
+        </div>
+    )
+}
+
+export default TestsPage
