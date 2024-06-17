86f1584cbf991db3105e21a1161283d3e42802aa
José Teles <josetelesmaciel@gmail.com>
2024-06-17 19:50:17 -0300

    refactor: Improve TestsPage component by making children span element a flex container
    This commit improves the TestsPage component by making the children span element inside the DraggableButton component a flex container. By adding the "flex" class to the span element, we enable flexbox layout for the children, allowing them to be properly aligned and spaced. This change enhances the visual presentation and flexibility of the DraggableButton component.

--- src/app/tests/page.tsx
+++ src/app/tests/page.tsx
@@ -71,7 +71,7 @@ const dndId = {
 const DraggableButton: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     return (
         <div className="p-2 mb-2 bg-white rounded border inline-flex w-full items-center">
-            <span className="mr-2 w-full">{children}</span>
+            <span className="mr-2 w-full flex">{children}</span>
             <span className={`transition bg-blue-400 hover:bg-blue-500 ml-auto text-white p-1 font-semibold text-xs rounded-sm`}>drag</span>
         </div>
     )
